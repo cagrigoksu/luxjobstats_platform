4a405bd68ee6807e024a2b770958868a0c2b8e11
Cagri Goksu Ustundag <cagrigoksu.ustundag@gmail.com>
2025-12-02 04:44:04 +0100

    Cleanup: fully remove build files and apply correct gitignore

--- luxjobstats-auth/src/AuthService/Program.cs
+++ luxjobstats-auth/src/AuthService/Program.cs
@@ -95,16 +95,16 @@ builder.Services.AddScoped<RefreshTokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} 
 
 //* app. A > Z
-app.MapControllers();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
